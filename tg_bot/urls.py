0255ae9d6288c8447f2124d4929af4c293908990
Kumar Akhmadiyev <k.akhmadiev@globerce.com>
2022-04-17 22:13:25 +0600

    Hotfix for import

--- tg_bot/urls.py
+++ tg_bot/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from tg_bot.views import MessageViewSet, TGWebHookView
+from tg_bot.views import MessageViewSet, TGBotWebHookView
 
 
 urlpatterns = [
     path('list/', MessageViewSet.as_view({'get': 'list'}), name='message-list'),
     path('send/', MessageViewSet.as_view({'post': 'create'}), name='create-message'),
-    path('webhook/<str:token>/', TGWebHookView.as_view(), name='webhook')
+    path('webhook/<str:token>/', TGBotWebHookView.as_view(), name='webhook')
 ]
